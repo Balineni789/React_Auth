41a4bafe6ad07147ca4b885b879fb74d992e2530
Balineni Harsha Sai <167176992+Balineni789@users.noreply.github.com>
2025-12-11 20:54:03 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,4 @@
 
-// import React, { useState } from 'react'
-// import { ToastContainer } from 'react-toastify'
-// import 'react-toastify/dist/ReactToastify.css'
-// import Login from './LoginForm/Login'
-// import Register from './LoginForm/Register'
-// import Home from './LoginForm/Home';
-
-// const App = () => {
-//   const [isLogin, setIsLogin] = useState(true)
-
-//   return (
-//     <div>
-//       <ToastContainer
-//         position="top-right"
-//         autoClose={3000}
-//         hideProgressBar={false}
-//         newestOnTop={true}
-//         closeOnClick
-//         rtl={false}
-//         pauseOnFocusLoss
-//         draggable
-//         pauseOnHover
-//       />
-//       {isLogin ? (
-//         <Login onSwitchToRegister={() => setIsLogin(false)} />
-//       ) : (
-//         <Register onSwitchToLogin={() => setIsLogin(true)} />
-//       )}
-//     </div>
-//   )
-// }
-
-// export default App
-
-
-
-
-
-
 import React, { useState } from 'react'
 import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify'
